93c19dbf5e505a2e518dd302e5a5e7c859c62c4c
Reza Salimi <rezasalimiofficiall@gmail.com>
2025-03-17 20:52:08 +0330

    Added feature to delete all todos when user click on delete all button

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
 const alertMessage = document.getElementById("alert-message");
 const todosBody = document.querySelector("tbody");
+const deleteAllButton = document.getElementById("delete-all-button");
 
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 const saveToLocalStorage = () => {
   localStorage.setItem("todos", JSON.stringify(todos));
 };
@@ -76,5 +78,17 @@ const addHandler = () => {
   }
 };
 
-window.addEventListener("load", displayTodos)
+const deleteAllHandler = () => {
+  if (todos.length) {
+    todos = [];
+    saveToLocalStorage();
+    displayTodos();
+    showAlert("All todos cleared successfully ✅", "success")
+  } else {
+    showAlert("No todos to clear ❗", "error")
+  }
+};
+
+window.addEventListener("load", displayTodos);
 addButton.addEventListener("click", addHandler);
+deleteAllButton.addEventListener("click", deleteAllHandler);
